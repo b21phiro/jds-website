a359def732b48df83ba98625402c4b5b05197f72
rdunder <r.dunder@gmail.com>
2024-10-01 10:39:16 +0200

    testing the button on homeView

--- src/core/views/pages/homeView.js
+++ src/core/views/pages/homeView.js
@@ -1,11 +1,19 @@
+//import {button} from "../components/Button.js";
+import button from "../components/buttons.js";
+
 function layout() {
-    return `
+    return /*html*/ `
     <h1>Hello world!!!</h1>
     <section>
         <h2>Comments</h2>
           <ul id="comments"></ul>
+          ${button.buttonPrimary("Join the Community")}
     </section>
     `;
 }
 
-export default { layout };
+function afterRender() {
+    button.afterRender();
+}
+
+export default { layout, afterRender };
