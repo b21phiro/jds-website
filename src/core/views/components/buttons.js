1d1f4b6d4eb8faa369fc1f3c46044e789769b103
rdunder <r.dunder@gmail.com>
2024-10-01 11:29:22 +0200

    done with first itteration of primary button

--- src/core/views/components/buttons.js
+++ src/core/views/components/buttons.js
@@ -1,16 +1,25 @@
 import "../../../css/buttons.css";
 
-
-function buttonPrimary(text) {
+/**
+ *  Creates a button element
+ *  @param text { String }
+ *  @param id {string}
+ *  @return a button element
+ */
+function buttonPrimary(text, id) {
     return /*HTML*/ `
-    <button class="btn btn-primary">${text}</button>
+    <button id=${id} class="btn btn-primary">${text}</button>
     `;
 }
 
-function afterRender() {
-    document.querySelector(".btn-primary").addEventListener("click", () => {
-        alert("button clicked");
-    })
+
+/**
+ *  Creates a eventlistener on the button thats being created
+ *  @param id { String } button id
+ *  @param callbackFunc { function } the callback function
+ */
+function afterRender(id, callbackFunc) {
+    document.querySelector(`#${id}`).addEventListener("click", callbackFunc);
 }
 
 
--- src/core/views/pages/homeView.js
+++ src/core/views/pages/homeView.js
@@ -1,4 +1,3 @@
-//import {button} from "../components/Button.js";
 import button from "../components/buttons.js";
 
 function layout() {
@@ -7,13 +6,20 @@ function layout() {
     <section>
         <h2>Comments</h2>
           <ul id="comments"></ul>
-          ${button.buttonPrimary("Join the Community")}
+          
+    </section>
+    
+    <section>
+        <p>Demonstrates a button</p>
+        ${button.buttonPrimary("Join the Community", "btn-join-community")}
     </section>
     `;
 }
 
 function afterRender() {
-    button.afterRender();
+    button.afterRender( "btn-join-community", () => {
+        alert("button clicked"); 
+    });
 }
 
 export default { layout, afterRender };
