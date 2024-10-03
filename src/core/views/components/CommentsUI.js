9fc5cc23b724e0aaea210283fe04547a8ab23913
rdunder <r.dunder@gmail.com>
2024-10-03 18:49:58 +0200

    added initial files needed and basic structure

---
+++ src/core/views/components/CommentsUI.js
@@ -0,0 +1,20 @@
+import "../../../css/comments.css";
+import "../../models/comment.js";
+
+
+/**
+ *  Creates a comment ui element
+ * @param ??
+ *  @return a comment element
+ */
+function comment() {
+
+}
+
+
+/**
+ *  Happens after comment element has been rendered
+ */
+function afterRender() {
+
+}
