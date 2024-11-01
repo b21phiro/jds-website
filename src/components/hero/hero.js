7db1459c4258bd7eb862e60b382afe3ee3dce6ff
b21phiro <philip.rosenqvist@outlook.com>
2024-11-01 11:39:05 +0100

    a

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,8 +1,14 @@
+import './hero.css';
+
 const HERO_ELEMENT_ID = "hero";
 
 function layout() {
     return /*HTML*/ `
-    <section id="${HERO_ELEMENT_ID}"></section>
+    <section id="${HERO_ELEMENT_ID}">
+        <section class="container">
+            <h1 class="${HERO_ELEMENT_ID}-title">Tillsammans är vi Sveriges <span>största</span> juniorutvecklare</h1>
+        </section>
+    </section>
     `;
 }
 
