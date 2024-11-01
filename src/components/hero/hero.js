bfdaebdeca3c472bf4d9386968379f8656603580
b21phiro <philip.rosenqvist@outlook.com>
2024-11-01 15:45:00 +0100

    Added text and cta for landing

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,4 +1,7 @@
 import './hero.css';
+import { cta } from './../../components/button/button.js';
+import ArrowRight from './../../components/icon/arrowRight.js';
+import { Config } from './../../core/config.js';
 
 const HERO_ELEMENT_ID = "hero";
 
@@ -7,6 +10,12 @@ function layout() {
     <section id="${HERO_ELEMENT_ID}">
         <section class="container">
             <h1 class="${HERO_ELEMENT_ID}-title">Tillsammans är vi Sveriges <span>största</span> juniorutvecklare</h1>
+            ${cta({ 
+                href: Config.DISCORD_LINK, 
+                icon: ArrowRight(), 
+                text: "Gå med i discord", 
+                title: "Gå med i våran discord grupp"
+            })}
         </section>
     </section>
     `;
