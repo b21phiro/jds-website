bda55ffcdfa02390d0a307677f45bbf5d7349302
b21phiro <philip.rosenqvist@outlook.com>
2024-11-01 10:44:20 +0100

    Implemented header

--- src/core/config.js
+++ src/core/config.js
@@ -1,3 +1,9 @@
 export const Config = {
-    APP_TITLE: "JDS - Webbsida"
+    
+    APP_TITLE: "JDS - Webbsida",
+
+    DISCORD_LINK: "https://discord.gg/cJdDc4e2y9",
+    LINKEDIN_LINK: "https://www.linkedin.com/company/junior-dev-sverige",
+    INSTAGRAM_LINK: ""
+
 };
