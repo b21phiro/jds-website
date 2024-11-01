c590e4f48d98de725aa4e11defb9c4f937d049c8
b21phiro <philip.rosenqvist@outlook.com>
2024-11-01 11:16:35 +0100

    Implemented hero layout

---
+++ src/components/hero/hero.js
@@ -0,0 +1,9 @@
+const HERO_ELEMENT_ID = "hero";
+
+function layout() {
+    return /*HTML*/ `
+    <section id="${HERO_ELEMENT_ID}"></section>
+    `;
+}
+
+export { layout };
--- src/core/view.js
+++ src/core/view.js
@@ -1,11 +1,12 @@
 import * as Header from '../components/header/header.js';
 import * as Footer from '../components/footer/footer.js';
+import * as Hero from '../components/hero/hero.js';
 
 function layout() {
     return /*HTML*/ `
     ${ Header.layout() }
     <main class="main">
-    <h1>Header</h1>
+        ${ Hero.layout() }
     </main>
     ${ Footer.layout() }
     `;
