e537dd700e9cd56d5545edc961b2ffd5667c7c34
b21phiro <philip.rosenqvist@outlook.com>
2024-11-13 09:35:55 +0100

    Extracted icons

--- src/components/accordion/accordionArticle.js
+++ src/components/accordion/accordionArticle.js
@@ -1,23 +1,24 @@
-import * as chevron from './chevron-logo';
+import { Color } from '../../core/config';
+import ChevronDown from './../icon/chevronDown.js';
 
 function layout(clasSuffix, title, content) {
 
     return /*html*/ `
-    
     <article  class="article-${clasSuffix}">
         <div class="container">
             <div data-action="toggle-article" class="title">
                 <h2>${title}</h2>
-                <button class="btn">${chevron.layout()}</button>
+                <button class="btn">
+                    ${ ChevronDown({ color: Color.WHITE }) }
+                </button>
             </div>
             <div class="content">
                 <p>${content}</p>
-                <div class="temp-image"></div>
+                <figure class="temp-image"></figure>
             </div>
         </div>
     </article>
-
-    `
+    `;
 }
 
-export {layout}
+export { layout };
