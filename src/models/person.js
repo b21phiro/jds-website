a180ce8b5db02366d4c75553dbfb0c953b38e930
b21phiro <philip.rosenqvist@outlook.com>
2024-11-14 11:21:55 +0100

    Added better shuffle algo

--- src/models/person.js
+++ src/models/person.js
@@ -46,12 +46,20 @@ class People
 
                 // Shuffles the returning array.
                 if (options?.shuffle) {
-                    result.sort((a, b) => 0.5 - Math.random());
+                    this._shuffle(result);
                 }
 
             });
         return result;
     }
+
+    static _shuffle(array) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
 }
 
 export { Person, People }
