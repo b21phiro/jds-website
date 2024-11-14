09ea5fab82f67ccc8f825a7cab4de1d72d73d850
b21phiro <philip.rosenqvist@outlook.com>
2024-11-14 14:29:30 +0100

    Removed sanbox-canvas

--- src/core/app.js
+++ src/core/app.js
@@ -1,7 +1,6 @@
 import './app.css';
 import { Config } from './config.js';
 import * as View from './view.js';
-import * as Sandbox from './../components/hero/sandbox.js';
 
 const ROOT_ELEMENT_ID = "app";
 
@@ -19,8 +18,6 @@ function init()  {
     }
 
     rootElement.insertAdjacentHTML('afterbegin', View.layout());
-
-    Sandbox.init();
 
     View.onReady();
     
